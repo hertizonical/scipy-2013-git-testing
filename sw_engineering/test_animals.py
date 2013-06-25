372958890bd2f0abf16f116add363fd7dc458a3f
Ryan Alberson <ralberson@protection-consultants.com>
2013-06-25 10:59:53 -0500

    Added Test for mean function.

--- sw_engineering/test_animals.py
+++ sw_engineering/test_animals.py
@@ -2,4 +2,33 @@ import animals
 
 
 def test_read_animals():
-    pass
+    ref_dates = ['2011-04-22','2011-04-23','2011-04-23','2011-04-23','2011-04-23']
+    ref_times = ['21:06','14:12','10:24','20:08','18:46']
+    ref_species = ['Grizzly','Elk','Elk','Wolverine','Muskox']
+    ref_counts=[36,25,26,31,20]
+    
+    dates, times, species, counts = animals.read_animals('animals.txt')
+    
+    assert dates == ref_dates
+    assert times == ref_times
+    assert species == ref_species
+    assert counts == ref_counts
+    
+def test_mean():
+    nums = [1,2,3,4,5,6,7,8,9,10]
+    
+    mean = animals.mean(nums)
+    
+    assert mean == 5.5
+    
+    nums = [1,2,3,4,-5.5,6,7,8,9,10.1]
+    
+    mean = animals.mean(nums)
+    
+    assert mean == 4.46
+    
+    nums = [1,2,3,4,5.5,6,7,8,9,a]
+    
+    mean = animals.mean(nums)
+    
+
